7e2a56e7ee3f0a15bb411e0073ead82eb1a27ca7
khamimtw <66464601+khamimtw@users.noreply.github.com>
2021-01-11 21:21:24 +0700

    scroll

--- lib/login.dart
+++ lib/login.dart
@@ -28,199 +28,206 @@ class _LoginscreenState extends State<Loginscreen> {
               begin: Alignment.bottomCenter,
               end: Alignment.topCenter),
         ),
-        child: Form(
-          key: _formKey,
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 180,
-                ),
-                Row(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Text(
-                      'Welcome To Work.in',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 35),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: <Widget>[
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Text(
-                      'Sign in with your account',
-                      style: TextStyle(color: Colors.white, fontSize: 16),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 65,
-                ),
-                TextFormField(
-                  validator: (String value) {
-                    if (value.isEmpty) {
-                      return 'Harap Masukin Email/Phone';
-                    }
-                    return null;
-                  },
-                  cursorColor: Colors.white,
-                  style: TextStyle(color: Colors.white),
-                  maxLength: 16,
-                  maxLengthEnforced: true,
-                  controller: myUsernameController,
-                  decoration: InputDecoration(
-                    disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    hintText: '  Email/Phone',
-                    hintStyle: TextStyle(
-                        fontSize: 18,
-                        letterSpacing: 1.5,
-                        color: Colors.white70,
-                        fontWeight: FontWeight.w900),
-                    filled: true,
-                    hoverColor: Colors.transparent,
-                    focusColor: Colors.transparent,
-                    fillColor: Colors.white.withOpacity(.3),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(25),
-                    ),
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+          ),
+          child: Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 180,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                TextFormField(
-                  validator: (String value) {
-                    if (value.isEmpty) {
-                      return '   Harap Masukin Password';
-                    }
-                    return null;
-                  },
-                  cursorColor: Colors.white,
-                  style: TextStyle(color: Colors.white),
-                  maxLength: 16,
-                  maxLengthEnforced: true,
-                  controller: myPasswordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    hintText: '  Password',
-                    hintStyle: TextStyle(
-                        fontSize: 18,
-                        letterSpacing: 1.5,
-                        color: Colors.white70,
-                        fontWeight: FontWeight.w900),
-                    filled: true,
-                    hoverColor: Colors.transparent,
-                    focusColor: Colors.transparent,
-                    fillColor: Colors.white.withOpacity(.3),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.transparent),
-                      borderRadius: BorderRadius.circular(25),
-                    ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Text(
+                        'Welcome To Work.in',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 35),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        child: Text(
-                          'Lupa Password?',
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.w700),
-                        ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 40,
+                      ),
+                      Text(
+                        'Sign in with your account',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 65,
+                  ),
+                  TextFormField(
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return 'Harap Masukin Email/Phone';
+                      }
+                      return null;
+                    },
+                    cursorColor: Colors.white,
+                    style: TextStyle(color: Colors.white),
+                    maxLength: 16,
+                    maxLengthEnforced: true,
+                    controller: myUsernameController,
+                    decoration: InputDecoration(
+                      disabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      hintText: '  Email/Phone',
+                      hintStyle: TextStyle(
+                          fontSize: 18,
+                          letterSpacing: 1.5,
+                          color: Colors.white70,
+                          fontWeight: FontWeight.w900),
+                      filled: true,
+                      hoverColor: Colors.transparent,
+                      focusColor: Colors.transparent,
+                      fillColor: Colors.white.withOpacity(.3),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.circular(25),
                       ),
                     ),
-                    SizedBox(
-                      width: 40,
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 25, right: 25),
-                  child: ButtonTheme(
-                      buttonColor: Colors.white,
-                      minWidth: MediaQuery.of(context).size.width,
-                      height: 55,
-                      child: RaisedButton(
-                        onPressed: () {
-                          if (_formKey.currentState.validate()) {
-                            Navigator.pushNamed(context, '/');
-                          }
-                          return false;
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    validator: (String value) {
+                      if (value.isEmpty) {
+                        return '   Harap Masukin Password';
+                      }
+                      return null;
+                    },
+                    cursorColor: Colors.white,
+                    style: TextStyle(color: Colors.white),
+                    maxLength: 16,
+                    maxLengthEnforced: true,
+                    controller: myPasswordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      disabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      hintText: '  Password',
+                      hintStyle: TextStyle(
+                          fontSize: 18,
+                          letterSpacing: 1.5,
+                          color: Colors.white70,
+                          fontWeight: FontWeight.w900),
+                      filled: true,
+                      hoverColor: Colors.transparent,
+                      focusColor: Colors.transparent,
+                      fillColor: Colors.white.withOpacity(.3),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          child: Text(
+                            'Lupa Password?',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 40,
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 25, right: 25),
+                    child: ButtonTheme(
+                        buttonColor: Colors.white,
+                        minWidth: MediaQuery.of(context).size.width,
+                        height: 55,
+                        child: RaisedButton(
+                          onPressed: () {
+                            if (_formKey.currentState.validate()) {
+                              Navigator.pushNamed(context, '/');
+                            }
+                            return false;
+                          },
+                          child: Text(
+                            'Log in',
+                            style: TextStyle(color: Colors.grey, fontSize: 22),
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Don't Have an Accout ?",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/signup');
                         },
                         child: Text(
-                          'Log in',
-                          style: TextStyle(color: Colors.grey, fontSize: 22),
+                          'Sign up',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white),
                         ),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                      )),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: 25,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Don't Have an Accout ?",
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/signup');
-                      },
-                      child: Text(
-                        'Sign up',
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white),
-                      ),
-                    )
-                  ],
-                )
-              ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/signup.dart
+++ lib/signup.dart
@@ -15,76 +15,82 @@ class SignUpScreen extends StatelessWidget {
               begin: Alignment.bottomCenter,
               end: Alignment.topCenter),
         ),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 180,
-              ),
-              Row(
-                children: <Widget>[
-                  SizedBox(
-                    width: 40,
-                  ),
-                  Text(
-                    'Create Account',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 35),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 110,
-              ),
-              CustomTextField(
-                hint: '    enter your name',
-                issecured: false,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              CustomTextField(
-                hint: '    enter your Email',
-                issecured: false,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              CustomTextField(
-                hint: '    enter your Phone',
-                issecured: false,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              CustomTextField(
-                hint: '    enter your Password',
-                issecured: true,
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 25, right: 25),
-                child: ButtonTheme(
-                    buttonColor: Colors.white,
-                    minWidth: MediaQuery.of(context).size.width,
-                    height: 55,
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/login');
-                      },
-                      child: Text(
-                        'Create',
-                        style: TextStyle(color: Colors.grey, fontSize: 22),
-                      ),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25)),
-                    )),
-              ),
-            ],
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+          ),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 180,
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Text(
+                      'Create Account',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 110,
+                ),
+                CustomTextField(
+                  hint: '    enter your name',
+                  issecured: false,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomTextField(
+                  hint: '    enter your Email',
+                  issecured: false,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomTextField(
+                  hint: '    enter your Phone',
+                  issecured: false,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomTextField(
+                  hint: '    enter your Password',
+                  issecured: true,
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25, right: 25),
+                  child: ButtonTheme(
+                      buttonColor: Colors.white,
+                      minWidth: MediaQuery.of(context).size.width,
+                      height: 55,
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/login');
+                        },
+                        child: Text(
+                          'Create',
+                          style: TextStyle(color: Colors.grey, fontSize: 22),
+                        ),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25)),
+                      )),
+                ),
+              ],
+            ),
           ),
         ),
       ),
