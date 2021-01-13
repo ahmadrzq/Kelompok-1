a736013aef92c775e268b74e5068d4fbbe66f37a
BukanChoki <55580042+ChokitoHaskar@users.noreply.github.com>
2021-01-14 00:07:28 +0700

    pengahapusan useless import

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_solo/customwidget/customtext.dart';
 import 'package:mobile_solo/customwidget/customtextfield.dart';
 
 class SignUpScreen extends StatelessWidget {
